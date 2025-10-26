894adbb535a1dfe73b5d6144af9c8f9954056d3b
Subhajit281 <subhajitsarkar281@gmail.com>
2025-10-27 00:01:40 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// ✨ ADD THESE LINES FOR ERUDA ✨
+// Only import and initialize Eruda if we are in development mode
+if (import.meta.env.DEV) {
+  import('eruda').then((eruda) => eruda.default.init());
+}
+// ✨ END OF ERUDA CODE ✨
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
